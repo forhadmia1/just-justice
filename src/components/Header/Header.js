2d6ffb8e5fc3fe4c0c09b40c07b2d2add295917d
forhadmia1 <forhad8613@gmail.com>
2022-04-17 00:03:47 +0600

    initialize the project and implement header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#features">Home</Nav.Link>
+                        <Nav.Link href="#features">Blogs</Nav.Link>
+                        <Nav.Link href="#pricing">About</Nav.Link>
+                        <Nav.Link href="#pricing">Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;

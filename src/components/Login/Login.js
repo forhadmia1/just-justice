f832e64a21cf48a0ca57bd88d4e68154d45ea2f9
forhadmia1 <forhad8613@gmail.com>
2022-04-17 21:23:13 +0600

    implement password reset

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import GoogleSignin from '../GoogleSignign/GoogleSignin';
-import './Login.css'
+import './Login.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
     const [
@@ -14,11 +16,14 @@ const Login = () => {
         error,
     ] = useSignInWithEmailAndPassword(auth);
 
+    const [sendPasswordResetEmail, sending, error2] = useSendPasswordResetEmail(auth);
+
     const [error1, setError1] = useState('hi');
 
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || '/';
+    //sign in handle
     const handleSignin = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
@@ -30,7 +35,7 @@ const Login = () => {
     if (user) {
         navigate(from, { replace: true })
     }
-
+    //error handle for login
     useEffect(() => {
         if (error) {
             if (error.code === 'auth/user-not-found') {
@@ -40,25 +45,41 @@ const Login = () => {
                 setError1('Incorrect password')
                 return;
             } else {
-                setError1('something went wrong')
+                setError1('something went wrong, Try again!')
                 return;
             }
         }
     }, [error]);
+    //password reset 
+    const emailRef = useRef('');
+
+    const passwordReset = async () => {
+        const email = emailRef.current.value;
+        if (!email) {
+            toast('Please enter an email!!')
+            return;
+        } else {
+            await sendPasswordResetEmail(email)
+            toast('Email sent successfully!')
+        }
+
+    }
+
 
     return (
         <div className='d-flex justify-content-center mt-5'>
             <div className='from-container'>
                 <h2 className='text-center mb-4'>Login</h2>
                 <form onSubmit={handleSignin}>
-                    <input type="email" name="email" placeholder='Email' required />
+                    <input ref={emailRef} type="email" name="email" placeholder='Email' required />
                     <input type="password" name="password" placeholder='Enter your password' required />
                     <p className='text-center text-danger'>{error && error1}</p>
                     <Button type='submit' className='w-100 mt-3'>Login</Button>
                 </form>
-                <button className='btn btn-link text-danger d-block mx-auto'>Forget password?</button>
+                <button onClick={passwordReset} className='btn btn-link text-danger d-block mx-auto mt-2'>Forget password?</button>
                 <p className='text-center mt-2 '><Link className='text-danger fw-bold text-decoration-none' to={'/register'}>Create an account?</Link></p>
                 <GoogleSignin />
+                <ToastContainer />
             </div >
         </div >
     );
